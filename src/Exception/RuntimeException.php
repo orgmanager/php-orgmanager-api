5215b586aeb3d206ee450fb110c583e1bb8c42e0
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-03-07 13:22:11 +0100

    Add Runtime Exception

---
+++ src/Exception/RuntimeException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace OrgManager\ApiClient\Exception;
+
+/**
+ * RuntimeException.
+ *
+ * @author Miguel Piedrafita <soy@miguelpiedrafita.com>
+ */
+class RuntimeException extends \RuntimeException implements ExceptionInterface
+{
+}
