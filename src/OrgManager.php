b18b72b2c360f340c3a0afd11f3530b5e7267d1d
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-03-23 21:46:20 +0100

    Add a function to regenerate token and (optionaly) set it

--- src/OrgManager.php
+++ src/OrgManager.php
@@ -126,6 +126,21 @@ class OrgManager
     {
         return $this->post('/join/'.$id, ['username' => $username]);
     }
+    
+     /**
+     * @param bool $set
+     *
+     * @return string
+     */
+    public function regenerateToken($set = true)
+    {
+        if ($set)
+        {
+           return $this->connect($this->get('/token/regenerate'));
+        }
+        
+        return $this->get('/token/regenerate');
+    }
 
     /**
      * @param string $resource
